2bb92cbf862d30e8cf4c044c803c7e8dc3a24f6f
fr05t77 <optimus7907@gmail.com>
2021-10-04 13:29:35 +0700

    juice: DotOS Bringup

--- dot_juice.mk
+++ dot_juice.mk
@@ -11,15 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common aosp stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_juice
+PRODUCT_NAME := dot_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
@@ -27,9 +27,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-#Fingerprint
+# Fingerprint
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
 BUILD_DESCRIPTION := redfin-user 11 RQ3A.210905.001 7511028 release-keys
-
-#PixelBlaster
-BLASTER_BUILD_TYPE := OFFICIAL

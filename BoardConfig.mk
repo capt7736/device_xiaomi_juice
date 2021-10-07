0ead7ba879681b4acb5e755c391fbcf633afb1d7
frost <50163698+fr05t77@users.noreply.github.com>
2021-10-07 17:24:09 +0700

    juice: use lld

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,6 +135,10 @@ BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 #TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-elf-
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := gengkapak
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+	HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument" \
+	AR=llvm-ar LD=ld.lld NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true

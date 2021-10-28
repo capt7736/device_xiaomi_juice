e99f10f2a41c28223a34c22fea630fd8293b25f8
mobx <67450638+mobxCode@users.noreply.github.com>
2021-10-28 18:31:13 +0700

    Update cafex_juice.mk

--- cafex_juice.mk
+++ cafex_juice.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common Project Radiant stuff.
-$(call inherit-product, vendor/cafex/config/common.mk)
+# Inherit common CAF-Extended configurations
+$(call inherit-product, vendor/extended/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true

7fc76dec43f6ca8a39b4f0435d9a4ae2d7784452
mobx <67450638+mobxCode@users.noreply.github.com>
2021-10-29 12:14:08 +0700

    Update and rename nusa_juice.mk to nad_juice.mk

--- nad_juice.mk
+++ nad_juice.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
 # Inherit common CAF-Extended configurations
-$(call inherit-product, vendor/nusa/config/common.mk)
+$(call inherit-product, vendor/nad/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
@@ -20,7 +20,7 @@ USE_PIXEL_CHARGING := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := nusa_juice
+PRODUCT_NAME := nad_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115

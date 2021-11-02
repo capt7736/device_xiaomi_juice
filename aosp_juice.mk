72ba141863431b43425251b7cc587ed8b95ccf50
mobx <67450638+mobxCode@users.noreply.github.com>
2021-11-02 20:15:42 +0700

    Update aosp_juice.mk

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -18,6 +18,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_juice

a0efcc76af39d52d1b8fdeb9880b8df6c5b60a36
mobx <67450638+mobxCode@users.noreply.github.com>
2021-11-07 13:09:58 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_juice.mk
+    $(LOCAL_DIR)/weeb_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_juice-user \
-    aosp_juice-userdebug \
-    aosp_juice-eng
+    weeb_juice-user \
+    weeb_juice-userdebug \
+    weeb_juice-eng

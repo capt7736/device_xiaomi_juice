2f172ec0e6f348c0adc06dd2cc28f34ea09d0308
mobx <67450638+mobxCode@users.noreply.github.com>
2021-11-07 13:13:53 +0700

    Update and rename aosp_juice.mk to weeb_juice.mk

--- weeb_juice.mk
+++ weeb_juice.mk
@@ -4,15 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit framework first
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common Weeb Project stuff
+$(call inherit-product, vendor/weeb/build/product/weeb_product.mk)
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
-
-# Inherit common CAF-Extended configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
@@ -24,7 +24,7 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 PRODUCT_NAME := aosp_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := sm6115
+PRODUCT_MODEL := SM6115
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

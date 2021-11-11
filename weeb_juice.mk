b850e7013b080748c5a1ed7044b3045e1253b210
mobx <67450638+mobxCode@users.noreply.github.com>
2021-11-11 15:10:36 +0700

    Update weeb_juice.mk

--- weeb_juice.mk
+++ weeb_juice.mk
@@ -11,20 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common Weeb Project stuff
-$(call inherit-product, vendor/weeb/build/product/weeb_product.mk)
+# Inherit some AEX stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := weeb_juice
+PRODUCT_NAME := aosp_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6115
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ3A.211001.001 7641976 release-key
